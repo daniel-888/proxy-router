e5194d5791d114349ae24ac3d3d56de19f2d3123
josh kean <joshkean@joshs-MacBook-Pro.local>
2021-12-01 16:58:08 -0600

    adding in unversioned msgbus code

---
+++ cmd/msgbus/dest.go
@@ -0,0 +1,137 @@
+package msgbus
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+
+	"gitlab.com/TitanInd/lumerin/lumerinlib"
+)
+
+type DestNetUrl string
+
+type DestID IDString
+
+const DEFAULT_DEST_ID DestID = "DefaultDestID"
+
+type Dest struct {
+	ID     DestID
+	NetUrl DestNetUrl
+}
+
+//---------------------------------------------------------------
+//
+//---------------------------------------------------------------
+func (ps *PubSub) DestPubWait(dest Dest) (d Dest, err error) {
+
+	if dest.ID == "" {
+		dest.ID = DestID(GetRandomIDString())
+	}
+
+	event, err := ps.PubWait(DestMsg, IDString(dest.ID), dest)
+	if err != nil || event.Err != nil {
+		panic(fmt.Sprintf(lumerinlib.Funcname()+"Unable to add Record %s, %s\n", err, event.Err))
+	}
+
+	d = event.Data.(Dest)
+	if err != nil || event.Err != nil {
+		fmt.Printf(lumerinlib.Funcname()+" PubWait returned err: %s, %s\n", err, event.Err)
+	}
+
+	return d, err
+}
+
+//---------------------------------------------------------------
+//
+//---------------------------------------------------------------
+func (ps *PubSub) DestGetWait(id DestID) (dest *Dest, err error) {
+
+	if id == "" {
+		panic(fmt.Sprintf(lumerinlib.FileLine() + " empty DestID passed in\n"))
+	}
+
+	event, err := ps.GetWait(DestMsg, IDString(id))
+	if err != nil || event.Err != nil {
+		fmt.Printf(lumerinlib.Funcname()+" ID not found %s, %s\n", err, event.Err)
+	}
+
+	if event.Data == nil {
+		dest = nil
+	} else {
+		d := event.Data.(Dest)
+		dest = &d
+	}
+	return dest, err
+}
+
+//---------------------------------------------------------------
+//
+//---------------------------------------------------------------
+func (ps *PubSub) DestExistsWait(id DestID) bool {
+
+	dest, _ := ps.DestGetWait(id)
+
+	return dest != nil
+}
+
+//---------------------------------------------------------------
+//
+//---------------------------------------------------------------
+func (d *Dest) Host() (host string) {
+
+	u, err := url.Parse(string(d.NetUrl))
+	if err != nil {
+		panic(fmt.Sprintf(lumerinlib.FileLine()+"url: %s, err %s\n", u, err))
+	}
+
+	host, _, err = net.SplitHostPort(u.Host)
+	if err != nil {
+		panic(fmt.Sprintf(lumerinlib.FileLine()+"url: %s, err %s\n", u, err))
+	}
+
+	return host
+}
+
+//---------------------------------------------------------------
+//
+//---------------------------------------------------------------
+func (d *Dest) Port() (port string) {
+
+	u, err := url.Parse(string(d.NetUrl))
+	if err != nil {
+		panic(fmt.Sprintf(lumerinlib.FileLine()+"url: %s, err %s\n", u, err))
+	}
+
+	_, port, err = net.SplitHostPort(u.Host)
+	if err != nil {
+		panic(fmt.Sprintf(lumerinlib.FileLine()+"url: %s, err %s\n", u, err))
+	}
+
+	return port
+}
+
+//---------------------------------------------------------------
+//
+//---------------------------------------------------------------
+func (d *Dest) Username() string {
+
+	u, err := url.Parse(string(d.NetUrl))
+	if err != nil {
+		panic(fmt.Sprintf(lumerinlib.FileLine()+"url: %s, err %s\n", u, err))
+	}
+
+	return u.User.Username()
+}
+
+//---------------------------------------------------------------
+//
+//---------------------------------------------------------------
+func (d *Dest) Password() (string, bool) {
+
+	u, err := url.Parse(string(d.NetUrl))
+	if err != nil {
+		panic(fmt.Sprintf(lumerinlib.FileLine()+"url: %s, err %s\n", u, err))
+	}
+
+	return u.User.Password()
+}
---
+++ cmd/msgbus/dest_test.go
@@ -0,0 +1,96 @@
+package msgbus
+
+import (
+	"testing"
+)
+
+var ps *PubSub = New(1)
+var host = "127.0.0.1"
+var port = "3334"
+var username = "someusername"
+var testurl = "stratum+tcp://" + username + ":@" + host + ":" + port + "/"
+
+func TestDestPubWait(t *testing.T) {
+
+	var dest Dest
+	dest.ID = DestID(GetRandomIDString())
+	dest.NetUrl = DestNetUrl(testurl)
+
+	d, err := ps.DestPubWait(dest)
+
+	if err != nil {
+		t.Errorf("DestPubWait returned error: %s\n", err)
+	}
+
+	if d.ID != dest.ID {
+		t.Errorf("DestPubWait returned wrong ID: %s != %s\n", d.ID, dest.ID)
+	}
+
+}
+
+// func (ps *PubSub) DestGetWait(id DestID) (dest *Dest, err error) {
+func TestDestGetWait(t *testing.T) {
+
+	var dest Dest
+	dest.ID = DestID(GetRandomIDString())
+	dest.NetUrl = DestNetUrl(testurl)
+
+	_, err := ps.DestPubWait(dest)
+
+	if err != nil {
+		t.Errorf("DestPubWait returned error: %s\n", err)
+	}
+
+	destget, err := ps.DestGetWait(dest.ID)
+
+	if err != nil {
+		t.Errorf("DestPubWait returned error: %s\n", err)
+	}
+
+	if destget.ID != dest.ID {
+		t.Errorf("DestGetWait returned wrong ID: %s != %s\n", destget.ID, dest.ID)
+	}
+
+}
+
+func TestHost(t *testing.T) {
+
+	var dest Dest
+
+	dest.ID = DestID(GetRandomIDString())
+	dest.NetUrl = DestNetUrl(testurl)
+	h := dest.Host()
+
+	if host != h {
+		t.Errorf("Got %s, wanted %s", h, host)
+	}
+
+}
+
+func TestPort(t *testing.T) {
+
+	var dest Dest
+
+	dest.ID = DestID(GetRandomIDString())
+	dest.NetUrl = DestNetUrl(testurl)
+	p := dest.Port()
+
+	if port != p {
+		t.Errorf("Got %s, wanted %s", p, port)
+	}
+
+}
+
+func TestUsername(t *testing.T) {
+
+	var dest Dest
+
+	dest.ID = DestID(GetRandomIDString())
+	dest.NetUrl = DestNetUrl(testurl)
+	u := dest.Username()
+
+	if username != u {
+		t.Errorf("Got %s, wanted %s", u, username)
+	}
+
+}
