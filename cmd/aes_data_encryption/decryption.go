11971999e0d364fd5aeb0555a9d331a813947f54
josh kean <joshkean@joshs-MacBook-Pro.local>
2022-01-03 13:32:45 -0600

    added function for decryption of contract pool data

---
+++ cmd/aes_data_encryption/decryption.go
@@ -0,0 +1,18 @@
+package encryption
+
+import (
+	"C"
+	"encoding/hex"
+	"fmt"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/ecies"
+)
+
+
+func DecryptData(m string, k string) string {
+	privKey, _ := crypto.HexToECDSA(k)
+	privKeyECIES := ecies.ImportECDSA(privKey)
+	b, _ := hex.DecodeString(m)
+	decryptdata, _ := privKeyECIES.Decrypt(b, nil, nil)
+	return fmt.Sprintf("%s", decryptdata)
+}
