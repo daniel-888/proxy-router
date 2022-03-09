484c88d16b542f24ca1b80cb123959990407b6fa
josh kean <joshkean@joshs-MacBook-Pro.local>
2022-03-09 13:25:57 -0600

    added functionality to default parameter and close functions. Removed many tests and added new tests for closing and default parameter

--- cmd/lumerinnetwork/SIMPL/SIMPLE_test.go
+++ cmd/lumerinnetwork/SIMPL/SIMPLE_test.go
@@ -76,156 +76,100 @@ type ProtocolInterface interface {
 	EventHandler(*SimpleEvent)
 }
 
-//generate a simplestruct for testing purposes
+
+//generate a SimpleStruct for testing purposes
+func generateSimpleStruct() SimpleStruct {
+	myContext := generateTestContext()
+	mySimpleStruct := SimpleStruct{
+		ctx: myContext,
+		cancel: dummyFunc,
+		eventHandler: 0,
+		eventChan: make(chan SimpleEvent),
+		protocolChan: make(chan SimpleEvent),
+		commChan: make(chan []byte),
+	}
+	return mySimpleStruct
+}
+
+//generate a SimpleListenStruct for testing purposes
 func generateSimpleListenStruct() SimpleListenStruct {
 	myContext := generateTestContext()
 	myAddr := generateTestAddr()
-	myStruct, _ := New(myContext, myAddr)
+	myStruct, _ := New(myContext, myAddr, 0)
 	return myStruct
 }
 
 //generate a protocol layer for testing purposes
 func generateProtocolLayer() ProtocolLayer {
 	listenStruct := generateSimpleListenStruct()
-	simpleStruct, _ := NewSimpleStruct(generateTestContext())
+	simpleStruct := generateSimpleStruct()
 	return ProtocolLayer{
 		ListenStruct: listenStruct,
 		SimpleStruct: simpleStruct,
 	}
 }
 
-// this is a basic test to create a SimpleListenEvent
-// then have the connection layer request a SimpleStruct
-// then have the SimpleStruct initialize a ProtocolStruct
-// success will be determined by ensuring the communication layer
-// simple struct, and protocol layer know about eachother
 /*
 test steps
-1. protocol layer will initialize a SimpleListenStruct
-2. protocol layer calls the New function in the SIMPLE package
+1. protocol layer calls the New function in the SIMPLE package
 	a context and an Addr are passed into New
-3. protocol layer calls run method on SimpleListenStruct
+2. checks the error message, test fails if the error message is anything but nil
 
 */
 func TestInitializeSimpleListenStruct(t *testing.T) {
-	listenStruct, _ := New(generateTestContext(), generateTestAddr())
-	listenStruct.Run()
-
-}
-
-//send a message from the protocol layer to the simple layer
-func TestSendMessageFromProtocolToConnectionLayer(t *testing.T) {
-	/*
-		test steps
-		1. create simulated protocol layer
-		2. call function on listening struct to create a new simple struct
-		3. listen for simple struct on listen structs accept channel
-		4. initialize the event handler on the simple struct
-		5. create a dummy SimpleEvent (this could be extentiated outside of the test)
-		6. pass the SimpleEvent into the eventChan channel
-		7. close both structs
-
-		TODO check to see value of SimpleEvent within connection layer
-	*/
-	pc := generateProtocolLayer()
-	listenStruct := pc.ListenStruct
-	listenStruct.NewSimpleStruct(generateTestContext())
-	simpleStruct := <-listenStruct.accept
-	event := SimpleEvent{ //create a simpleEvent to pass into event chan
-		EventType: eventOne,
-		Data:      []byte{},
+	_, err := New(generateTestContext(), generateTestAddr(), 0)
+	if err != nil {
+		t.Error("failed to initialize SimpleListenStruct")
 	}
-	simpleStruct.EventHandler(event)
-	simpleStruct.Close()
+
+}
+
+/*
+test steps
+1. create a dummy communication layer to listen for information from a context
+2. create a new simple struct routine
+3. call the run function and pass in a standard context
+4. check the dummy communication layer to see if the context inforation has been passed fown
+*/
+func TestSimpleStructRun(t *testing.T) {
+	simpleStruct := generateSimpleStruct()
+	context := generateTestContext() //this needs to be replaced to accept a context from the protocol
+	simpleStruct.Run(context) //run is working but needs to do "something" with the context
+}
+
+/*
+test steps
+1. create a new SimpleListenStruct
+2. call the close function on the SimpleListenStruct
+3. ensure all associated routines are closed
+*/
+func TestSimpleListenStructClose(t *testing.T) {
+	listenStruct, _ := New(generateTestContext(), generateTestAddr(), 0)
 	listenStruct.Close()
+}
+
+/*
+test steps
+1. create a new SimpleListenStruct
+2. call the close function on the SimpleListenStruct
+3. ensure all associated routines are closed
+*/
+func TestSimpleStructClose(t *testing.T) {
+	simpleStruct := generateSimpleStruct()
+	context := generateTestContext() //this needs to be replaced to accept a context from the protocol
+	simpleStruct.Run(context) //run is working but needs to do "something" with the context
+	simpleStruct.Close()
+}
+
+func TestSetMessageSizeDefault(t *testing.T) {
+	simpleStruct := generateSimpleStruct()
+	if simpleStruct.maxMessageSize != 0 {
+		t.Errorf("message expected to be 0, actually is: %d", simpleStruct.maxMessageSize)
+	}
+	simpleStruct.SetMessageSizeDefault(100)
+	if simpleStruct.maxMessageSize != 100 {
+		t.Errorf("message expected to be 100, actually is: %d", simpleStruct.maxMessageSize)
+	}
 
 }
 
-//test to initialize a simple layer and protocol layer
-//connection layer will send byte information to the
-//SimpleStruct and the SimpleStruct will pass that information upwards
-//to the protocol layer
-//message will be byte array of string "test sentence one"
-func TestSendMessageFromConnectionLayer(t *testing.T) {
-	pc := generateProtocolLayer()
-	listenStruct := pc.ListenStruct
-	listenStruct.NewSimpleStruct(generateTestContext())
-	simpleStruct := <-listenStruct.accept
-	go simpleStruct.Run(listenStruct.ctx)
-
-	connLayer := NewConnLayer(simpleStruct)
-	connLayer.ConnToSimple()
-
-	// connMsg := <- simpleStruct.protocolChan
-	// fmt.Printf("Event Type: %s", connMsg.EventType)
-	//getting an issue where Data is being considered an Interface instead of a byetstring
-	// if "test sentence one" != string(connMsg.Data) {
-	//	t.Error("msg came out wrong",err)
-	//}
-}
-
-func TestReceiveMessageFromMSGBus(t *testing.T) {
-}
-
-// function to route a message from the connection layer to the protocol layer
-// this function creates a ConnectionMessage and specifies that it wants to push
-// it to the protocol layer
-// will be successful is message provided in ConnectionMessage is detected in the ProtocolChan
-func TestPushMessageToProtocol(t *testing.T) {
-}
-
-// function to route a message from the protocol layer to the msgbus
-// this function creates a ConnectionMessage and specifies that it wants to push
-// it to the protocol layer
-// will be successful is message provided in ConnectionMessage is detected in the ProtocolChan
-func TestPushMessageToMSGBus(t *testing.T) {
-}
-
-// function to route a message from the protocol layer to the connection layer
-// this function creates a ConnectionMessage and specifies that it wants to push
-// it to the protocol layer
-// will be successful is message provided in ConnectionMessage is detected in the ProtocolChan
-func TestPushMessageToConnectionLayer(t *testing.T) {
-}
-
-func TestHashrateCountMessage(t *testing.T) {
-}
-
-func TestValidationRequestMessage(t *testing.T) {
-}
-
-//send the following messages
-// 1. message from connection layer to protocol layer
-// 2. message from msg.bus to protocol layer
-// 3. message from protocol layer to msgbus
-// this test will be considered successful if the messages
-// are processed in order and also make it to their final destination
-func TestMessageFrom3Sources(t *testing.T) {
-}
-
-//send the following messages
-// 1. message from protocol to connection layer
-// 2. message from protocol to msgbus
-// this test will be considered successful if the messages are processed in order
-// and make it to their intended destinations
-func TestMultipleMessagesFromProtocol(t *testing.T) {
-}
-
-//send the following messages
-// 1. message from protocol to connection layer
-// 2. message from protocol to msgbus
-// this test will be considered successful if the messages are processed in order
-// and make it to their intended destinations
-func TestMultipleMessagesFromConnectionLayer(t *testing.T) {
-}
-
-// test to send a corrupted message through the SIMPLE layer
-// the corrputed message should go through as expected since
-// the simple layer doesn't check for message integrity
-func TestCorruptMessage(t *testing.T) {
-}
-
-//send a message from the connection channel with an option of 100
-//this will not be picked up by any cases in the processIncomingMessage function
-func TestMessageWithInvalidActions(t *testing.T) {
-}
